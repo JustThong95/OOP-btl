a16050c07059ec927521f75e7dafeec8e5676376
Parktring <xuanbac.0946516204@gmail.com>
2026-04-09 14:30:57 +0700

    Change database password in DBConnection.java
    Updated database password for security reasons.

--- src/main/java/com/stockmanager/db/DBConnection.java
+++ src/main/java/com/stockmanager/db/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
     // Modify these basic credentials if your local config differs
     private static final String URL = "jdbc:mysql://127.0.0.1:3307/stock_manager?useSSL=false&allowPublicKeyRetrieval=true";
     private static final String USER = "root";
-    private static final String PASSWORD = "thongchu95"; 
+    private static final String PASSWORD = "yourpassword"; 
 
     public static Connection getConnection() throws SQLException, ClassNotFoundException {
         // Ensure JDBC driver is registered
